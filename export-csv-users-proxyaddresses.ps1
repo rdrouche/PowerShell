4ad5a152f77cf48dbccce2079bdb7e0476cc9be3
Romain <37143778+rdrouche@users.noreply.github.com>
2018-03-12 10:52:20 +0100

    news scripts
    permet l export / import en csv des proxyaddresses

---
+++ export-csv-users-proxyaddresses.ps1
@@ -0,0 +1,17 @@
+﻿#
+#	@author: Romain Drouche
+#	@Description : Exporte le champs proxyAddresses des utilisateurs de l'OU cible $contactou
+#	@website : http://blog.rdr-it.col
+#	@original source : https://241931348f64b1d1.wordpress.com/2015/10/21/how-to-dump-users-proxyaddresses-attribute-with-powershell/ 
+#
+ipmo activedirectory
+cls 
+cd C:\
+$contactou = "OU=users ,DC=mondomaine,DC=intra"
+$datesuffix = get-date -Format yyy-MM-dd_HHmmss
+ 
+$allcontacts = get-adobject -filter {objectclass -eq "user" } -searchbase $contactou -property DistinguishedName,ObjectGUID,proxyaddresses
+ 
+$allcontacts | select DistinguishedName,ObjectGUID,@{Name='proxyAddresses';Expression={[string]::join(";", $($_.proxyAddresses))}} | export-csv -delimiter ";" -notype alldominocontacts_$($datesuffix).csv
+Write-Host "-----------------"
+Write-Host "Export termine"
---
+++ import-csv-users-proxyadresses.ps1
@@ -0,0 +1,22 @@
+﻿#
+#	@author: Romain Drouche
+#	@Description : Exporte le champs proxyAddresses des utilisateurs de l'OU cible $contactou
+#	@website : http://blog.rdr-it.col
+#	@original source : https://241931348f64b1d1.wordpress.com/2015/10/21/how-to-dump-users-proxyaddresses-attribute-with-powershell/ 
+#
+cls
+$contactou = "OU=SA SAGET,DC=RDR,DC=LAB"
+cd C:\
+Import-Csv "alldominocontacts.csv" -delimiter ";" | ForEach-Object{
+    $guid = $_.ObjectGUID
+    $proxyAddresses = $_.proxyaddresses -split ';'
+     
+    $find = Get-ADObject -filter {(objectGUID -eq $guid)} -searchbase $contactou -Properties Name,ProxyAddresses
+    Write-Host "Utilisateur:"
+    Write-Host $find.Name
+    Write-Host "Current ProxyAddresses:" $find.proxyaddresses
+    Write-Host "Old ProxyAddresses    :" $proxyAddresses
+    Set-ADObject -Identity $guid -Replace @{proxyAddresses=$proxyAddresses}    
+    Write-Host "-----------------"
+    Write-Host
+}
